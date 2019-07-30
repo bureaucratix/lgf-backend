04441603f8c72c92a1df5a11ba6064c7aa68757a
Bureaucratix <bureaucratix@gmail.com>
2019-07-30 10:12:02 -0700

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :plants
+    validates :username, length: { in: 3..12 }
+    validates :password, length: { in: 6..20 }
+    validates :name, :username, :email, presence: true
     has_secure_password
 end
